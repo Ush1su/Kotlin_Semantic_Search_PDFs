2440d38afc382e801f95feca21d9613e7ea81414
Ivan Osipenko <padavan2007@gmail.com>
2026-07-05 23:17:02 +0200

    added Chunker class and function to chunk parsed pdf file. Spring Boot dependency added as well.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
+    id("org.springframework.boot") version "4.0.1"
+    id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.3.21"
+    kotlin("plugin.spring") version "2.2.21"
 }
 
 group = "org.example"
@@ -12,6 +15,16 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.apache.pdfbox:pdfbox:3.0.7")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("tools.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jdbc-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
 
 kotlin {
---
+++ src/main/kotlin/org/ai_processor/embeddings/Chunker.kt
@@ -0,0 +1,56 @@
+package org.ai_processor.embeddings
+
+import org.ai_processor.pdfreader.ParsedPDF
+import org.ai_processor.pdfreader.TextBlock
+import org.springframework.stereotype.Component
+import java.util.UUID
+
+import kotlin.collections.ArrayDeque
+
+@Component
+class Chunker {
+    private val maxChunkSize = 1000
+    private val maxChunkOverlap = 200
+
+    fun chunkPDF(parsedPDF: ParsedPDF): List<PdfChunk> {
+        var currentChunkSize = 0
+        val chunks = mutableListOf<PdfChunk>()
+        val currentTextBlocks = ArrayDeque<TextBlock>()
+        var chunkIndex = 1
+        for (page in parsedPDF.pages) {
+            for (textBlock in page.textBlocks) {
+                if (currentChunkSize + textBlock.text.length > maxChunkSize) {
+                    chunks.add(buildPdfChunk(parsedPDF.documentId, chunkIndex, currentTextBlocks))
+                    chunkIndex += 1
+                    while (currentChunkSize > maxChunkOverlap) {
+                        val overlap = currentTextBlocks.removeFirst()
+                        currentChunkSize -= overlap.text.length
+                    }
+                }
+                currentTextBlocks.addLast(textBlock)
+                currentChunkSize += textBlock.text.length
+            }
+        }
+        chunks.add(buildPdfChunk(parsedPDF.documentId, chunkIndex, currentTextBlocks))
+        return chunks
+    }
+
+    private fun buildPdfChunk(documentId: UUID, chunkIndex: Int, currentTextBlocks: ArrayDeque<TextBlock>): PdfChunk {
+        val text = currentTextBlocks.joinToString(separator = " ") { it.text }
+        val highlightRectangles = currentTextBlocks.map {
+            HighlightRect(
+                pageNumber = it.pageNumber,
+                bbox = it.bbox
+            )
+        }
+        return PdfChunk(
+            id = UUID.randomUUID(),
+            documentId = documentId,
+            chunkIndex = chunkIndex,
+            text = text,
+            pageStart = currentTextBlocks.first().pageNumber,
+            pageEnd = currentTextBlocks.last().pageNumber,
+            highlightRects = highlightRectangles
+        )
+    }
+}
