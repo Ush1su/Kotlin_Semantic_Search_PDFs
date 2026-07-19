deffbd46a0621b19a1b09cec7b4acb32e0f95099
Ivan Osipenko <padavan2007@gmail.com>
2026-07-19 18:12:20 +0200

    added document search endpoint, service and working pipeline

--- src/main/kotlin/org/ai_processor/document/api/DocumentSearchController.kt
+++ src/main/kotlin/org/ai_processor/document/api/DocumentSearchController.kt
@@ -1,6 +1,8 @@
 package org.ai_processor.document.api
 
 import org.ai_processor.document.api.model.DocumentSearchResponse
+import org.ai_processor.document.api.model.DocumentSearchResultResponse
+import org.ai_processor.document.api.model.SearchChunkResponse
 import org.ai_processor.document.services.DocumentSearchService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,10 +21,23 @@ class DocumentSearchController(
         @RequestParam(defaultValue = "10") limit: Int,
         @RequestParam(required = false) minimumScore: Float?
     ): DocumentSearchResponse {
-        return documentSearchService.search(
+
+        val vectorSearchOutput = documentSearchService.search(
             query = query,
             limit = limit,
             minimumScore = minimumScore
         )
+        val results = vectorSearchOutput.map { match ->
+            DocumentSearchResultResponse(
+                score = match.score,
+                chunk = SearchChunkResponse(
+                    text = match.text,
+                    chunkId = match.chunkId,
+                    documentId = match.documentId
+                )
+            )
+        }
+
+        return DocumentSearchResponse(query = query, results = results)
     }
 }
--- src/main/kotlin/org/ai_processor/document/services/DocumentSearchService.kt
+++ src/main/kotlin/org/ai_processor/document/services/DocumentSearchService.kt
@@ -1,8 +1,26 @@
 package org.ai_processor.document.services
 
 import org.ai_processor.processing.embeddings.EmbeddingService
+import org.ai_processor.vector_storage.QdrantService
+import org.ai_processor.vector_storage.model.VectorSearchMatch
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
-class DocumentSearchService {
+class DocumentSearchService (
+    private val embeddingService: EmbeddingService,
+    private val qdrantService: QdrantService
+){
+
+    private val logger = LoggerFactory.getLogger(DocumentSearchService::class.java)
+
+    fun search(
+        query: String,
+        limit: Int = 10,
+        minimumScore: Float? = 0.5f
+    ) : List<VectorSearchMatch> {
+        val embedding = embeddingService.embedText(query)
+        logger.info("Embedding of $query is $embedding")
+        return qdrantService.searchByVector(embedding, limit, minimumScore)
+    }
 }
